c9fd1f7123d061dd4e75002aa533c1bd1cbfc627
Immow <k.schippers@gmail.com>
2022-10-20 17:51:48 +0200

    removed unused "states" to reduce code

--- buttons/5/state.lua
+++ buttons/5/state.lua
@@ -14,44 +14,8 @@ function State:draw()
 	if scene.draw then scene:draw() end
 end
 
-function State:keypressed(key,scancode,isrepeat)
-	if scene.keypressed then scene:keypressed(key,scancode,isrepeat) end
-end
-
-function State:keyreleased(key,scancode)
-	if scene.keyreleased then scene:keyreleased(key,scancode) end
-end
-
 function State:mousepressed(x,y,button,istouch,presses)
 	if scene.mousepressed then scene:mousepressed(x,y,button,istouch,presses) end
-end
-
-function State:mousereleased(x,y,button,istouch,presses)
-	if scene.mousereleased then scene:mousereleased(x,y,button,istouch,presses) end
-end
-
-function State:mousemoved(x, y, dx, dy, istouch)
-	if scene.mousemoved then scene:mousemoved(x, y, dx, dy, istouch) end
-end
-
-function State:wheelmoved(x, y)
-	if scene.wheelmoved then scene:wheelmoved(x, y) end
-end
-
-function State:touchpressed(id,x,y,dx,dy,pressure)
-	if scene.touchpressed then scene:touchpressed(id,x,y,dx,dy,pressure) end
-end
-
-function State:touchreleased(id,x,y,dx,dy,pressure)
-	if scene.touchreleased then scene:touchreleased(id,x,y,dx,dy,pressure) end
-end
-
-function State:touchmoved(id,x,y,dx,dy,pressure)
-	if scene.touchmoved then scene:touchmoved(id,x,y,dx,dy,pressure) end
-end
-
-function State:textinput(t)
-	if scene.textinput then scene:textinput(t) end
 end
 
 function State.setScene(nextScene)
